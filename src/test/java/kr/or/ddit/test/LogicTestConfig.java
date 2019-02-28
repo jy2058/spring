27306c1789df7acb707abc573d16803e9d084798
jy <jiy3285@gmail.com>
2019-02-28 11:07:43 +0900

    spring test 환경 수정
    1. controller테스트 코드 설정 분리
    2. service, dao 테스트 코드 설정 분리

--- src/test/java/kr/or/ddit/test/LogicTestConfig.java
+++ src/test/java/kr/or/ddit/test/LogicTestConfig.java
@@ -1,36 +1,14 @@
 package kr.or.ddit.test;
 
-import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:kr/or/ddit/config/spring/servlet-context.xml",
-					"classpath:kr/or/ddit/config/spring/application-context.xml"})
-@WebAppConfiguration	// 스프링 컨테이너를 만들 때 WepApplicationContext로 생성
-						// 미 적용시 applicationContext
+@ContextConfiguration({"classpath:kr/or/ddit/config/spring/application-context.xml"})
 
+// service, dao 테스트 할 때 상속하기
 public class LogicTestConfig {
-	@Autowired
-	private WebApplicationContext context;
-	protected MockMvc mockMvc;
 	
-	// @BeforeClass (static이어서 사용빈도가 떨어짐)
-	// 	@Before - @Test - @After
-	// 	@Before - @Test - @After
-	//	..........
-	// @AfterClass (static이어서 사용빈도가 떨어짐)
-
-	@Before
-	public void setUp(){
-		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-	}
-
 
 }
